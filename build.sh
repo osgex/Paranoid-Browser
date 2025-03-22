4d3ed988a28373b7fcc20d06c09e375ef126354b
osgex <200525879+osgex@users.noreply.github.com>
2025-03-22 11:32:57 +0000

    fix error

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -rf build/
 # Download the latest Firefox, skip if it is already downloaded
 wget --content-disposition --directory-prefix=bin --no-clobber 'https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US'
 # Get name and extract archive
-archive=$(ls bin/*.xz | head -1)
+archive=$(ls bin/*.xz -t | head -1)
 tar xvf $archive
 mv firefox build
 # Copy Paranoid Browser configuration
